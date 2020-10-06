9ad2fdecb944834e996e30b1c4fdcdcbf7683484
Gabbill <gabriel.billard@polymtl.ca>
2020-10-06 19:33:01 -0400

    le import marche pas

--- exercice.py
+++ exercice.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
+import math
+import sys
+from sys.path(1,"C:\Users\gabri\PycharmProjects\c04-ch6-exercices-Gabbill")
+from exercice2 import frequence
 
-
+#a)
 
 # TODO: Définissez vos fonction ici
-
+def volume_elypsoide(a=2,b=3,c=1,p=4):
+    volume=(4/3)*math.pi*a*b*c
+    mass = p * volume
+    return volume, mass
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    print(volume_elypsoide())
     pass
